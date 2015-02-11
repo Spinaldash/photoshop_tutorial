35b0d045b1c692b99f919ed58ebe92eab3767a59
Spinaldash <blueavenger@gmail.com>
2015-02-11 14:56:48 -0800

    random-backgrounds + canvas paint

--- client/main.js
+++ client/main.js
@@ -5,6 +5,54 @@ $(document).ready(init);
 function init() {
   $('#add-color').click(addColor);
   $('#add-random-colors').click(addRandom);
+  $('#start').click(clickStart);
+  $('#stop').click(clickStop);
+  $('#colors').on('click', '.box', clickBox);
+  $('#canvas').on('mouseenter', '.tiny', enterTiny);
+  $('#add-canvas').click(clickAddCanvas);
+}
+
+var colors = [];
+var timer;
+var index = 0;
+
+function enterTiny() {
+  var color = $('#selected').css('background-color');
+  $(this).css('background-color', color);
+}
+
+function clickAddCanvas() {
+  var size = $('#canvas-size').val() * 1;
+
+  for (var i = 0; i < size; i++) {
+    var $tiny = $('<div>');
+    $tiny.addClass('tiny');
+    $('#canvas').append($tiny);
+  }
+}
+
+function clickStart() {
+  colors = $('.box').toArray().map(function(element) {
+  return $(element).css('background-color');
+});
+
+  index = 0;
+  clearInterval(timer);
+  timer = setInterval(looping, 250);
+
+}
+
+function looping() {
+  var color = colors[index];
+  $('body').css('background-color', color);
+  index++;
+  if (index > colors.length) {
+    index = 0;
+  }
+}
+
+function clickStop() {
+  clearInterval(timer);
 }
 
 function addColor() {
@@ -18,24 +66,28 @@ function addColor() {
 
 }
 
-function addRandom(){
+function addRandom() {
   var quantity = $('#quantity').val() * 1;
-  for(var i = 0; i<quantity; i++){
+  for (var i = 0; i < quantity; i++) {
 
     var $box = $('<div>');
     $box.css('background-color', addRandomColor());
     $box.addClass('box');
 
     $('#colors').append($box);
-
   }
 
+}
+
+function clickBox() {
+  var color = $(this).css('background-color');
+  $('#selected').css('background-color', color);
 }
 
 function addRandomColor() {
   var fus = Math.floor(Math.random() * 256);
   var roh = Math.floor(Math.random() * 256);
   var dah = Math.floor(Math.random() * 256);
-  return "rgb(" + fus + "," + roh + "," + dah + ")"
+  return 'rgb(' + fus + ',' + roh + ',' + dah + ')';
 
 }
